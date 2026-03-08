a7b1d59789e2b81a20dffdb03773bae4c72113c2
IshankS640 <ishanksharma12012005@gmail.com>
2026-03-09 02:15:51 +0530

    add: create constituencies file manually

---
+++ contituencies.py
@@ -0,0 +1,7 @@
+STATE_CONSTITUENCY_MAP = {
+    "Andhra Pradesh": ["Kurnool", "Nandyal", "Vijayawada", "Visakhapatnam"],
+    "Delhi": ["Chandni Chowk", "New Delhi", "South Delhi", "East Delhi"],
+    "Karnataka": ["Bangalore Central", "Mysore", "Udupi"],
+    "Maharashtra": ["Mumbai North", "Pune", "Nagpur"],
+    "Uttar Pradesh": ["Lucknow", "Varanasi", "Amethi"]
+}
