4b4c58e51721846581eaba05b38598b29d1060f3
IshankS640 <ishanksharma12012005@gmail.com>
2026-03-10 02:56:26 +0530

    data: add full list of 2024 politicians to Candidate Map

--- constituencies.py
+++ constituencies.py
@@ -5,14 +5,53 @@ STATE_CONSTITUENCY_MAP = {
     "Maharashtra": ["Mumbai North", "Pune", "Nagpur"],
     "Uttar Pradesh": ["Lucknow", "Varanasi", "Amethi"]
 }
-# Add this to the bottom of constituencies.py
-
+# The updated, complete list of 2024 MPs for your app
 CANDIDATE_MAP = {
+    # Andhra Pradesh
     "Kurnool": "Bastipati Nagaraju",
+    "Nandyal": "Byreddy Shabari",
+    "Vijayawada": "Kesineni Sivanath",
+    "Visakhapatnam": "Sribharat Mathukumilli",
+    "Guntur": "Pemmasani Chandrasekhar",
+    "Tirupati": "Gurumoorthy Maddila",
+    
+    # Delhi
+    "Chandni Chowk": "Praveen Khandelwal",
+    "East Delhi": "Harsh Malhotra",
     "New Delhi": "Bansuri Swaraj",
+    "North East Delhi": "Manoj Tiwari",
+    "North West Delhi": "Yogender Chandoliya",
+    "South Delhi": "Ramvir Singh Bidhuri",
+    "West Delhi": "Kamaljeet Sehrawat",
+    
+    # Karnataka
+    "Bangalore Central": "P. C. Mohan",
+    "Bangalore North": "Shobha Karandlaje",
+    "Bangalore South": "Tejasvi Surya",
+    "Mysore": "Yaduveer Wadiyar",
+    "Udupi Chikmagalur": "Kota Srinivas Poojary",
+    
+    # Maharashtra
+    "Mumbai North": "Piyush Goyal",
+    "Mumbai South": "Arvind Sawant",
+    "Pune": "Murlidhar Mohol",
+    "Nagpur": "Nitin Gadkari",
+    "Thane": "Naresh Mhaske",
+    "Nashik": "Rajabhau Waje",
+    
+    # Telangana
+    "Adilabad": "Godam Nagesh",
+    "Hyderabad": "Asaduddin Owaisi",
+    "Karimnagar": "Bandi Sanjay Kumar",
+    "Khammam": "Ramasahayam Raghuram Reddy",
+    "Nizamabad": "Arvind Dharmapuri",
+    "Secunderabad": "G. Kishan Reddy",
+    
+    # Uttar Pradesh
+    "Agra": "S. P. Singh Baghel",
+    "Aligarh": "Satish Kumar Gautam",
+    "Amethi": "Kishori Lal Sharma",
     "Lucknow": "Rajnath Singh",
     "Varanasi": "Narendra Modi",
-    "Hyderabad": "Asaduddin Owaisi",
-    "Bangalore South": "Tejasvi Surya",
-    "Pune": "Murlidhar Mohol"
+    "Gorakhpur": "Ravi Kishan"
 }
