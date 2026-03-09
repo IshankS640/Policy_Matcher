d5ede9dd8bb206e04f29685c6c389f231a45d17e
IshankS640 <ishanksharma12012005@gmail.com>
2026-03-10 03:36:31 +0530

    fix: clean up deleted files and finalize app

--- contituencies.py
+++
@@ -1,7 +0,0 @@
-STATE_CONSTITUENCY_MAP = {
-    "Andhra Pradesh": ["Kurnool", "Nandyal", "Vijayawada", "Visakhapatnam"],
-    "Delhi": ["Chandni Chowk", "New Delhi", "South Delhi", "East Delhi"],
-    "Karnataka": ["Bangalore Central", "Mysore", "Udupi"],
-    "Maharashtra": ["Mumbai North", "Pune", "Nagpur"],
-    "Uttar Pradesh": ["Lucknow", "Varanasi", "Amethi"]
-}
